76c5399949f10df57617da69b68b4e88e623e1f6
abhi-develope <prajapatiabhishek320@gmail.com>
2024-05-20 05:30:43 +0530

    array method

--- Array/index.js
+++ Array/index.js
@@ -3,4 +3,16 @@ const arr = ['a','b','c','d']
 
 
 //push
-arr.push('e');
+arr.push('e'); // O(1)
+
+//pop
+arr.pop();     // O(1)
+
+console.log(arr);
+
+//unshift
+arr.unshift('x')  // O(n) because its change or shift all the index of the previous element of the array
+
+//splice
+arr.splice(2, 0, 'allien');  // O(n)
+
