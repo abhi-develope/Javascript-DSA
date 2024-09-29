510f3520f9f8dcd623ef104a4e4e85fe0d6ee0ed
abhi-develope <prajapatiabhishek320@gmail.com>
2024-09-29 22:59:42 +0530

    arraySum

---
+++ basicPracticeProblems/arraySum.js
@@ -0,0 +1,13 @@
+// Create an array of numbers, then find the sum of all elements.
+
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+let sum = 0;
+
+for(let i=0; i<arr.length; i++){
+    sum += arr[i];
+}
+
+
+console.log(sum)
