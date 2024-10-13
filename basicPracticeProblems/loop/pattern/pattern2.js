949f069dbe0345a892ad4900a28ac17cf6aefb0c
abhi-develope <prajapatiabhishek320@gmail.com>
2024-10-13 22:36:25 +0530

    update second method

--- basicPracticeProblems/loop/pattern/pattern2.js
+++ basicPracticeProblems/loop/pattern/pattern2.js
@@ -17,3 +17,15 @@ for(let i =0; i<n; i++){
    
     
 }
+
+
+// method 2 , no need to take extra variable due console new line
+
+const num = 6;
+
+for (let i = 0; i < num; i++) {
+    for (let j = 0; j < i + 1; j++) {
+        process.stdout.write("* ");  // Print stars on the same line
+    }
+    console.log();  // Print a newline after the row is completed
+}
