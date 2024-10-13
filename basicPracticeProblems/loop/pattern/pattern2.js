034b455a21ca378162547ba84490c80ee88930d5
abhi-develope <prajapatiabhishek320@gmail.com>
2024-10-13 18:24:07 +0530

    pattern 2

---
+++ basicPracticeProblems/loop/pattern/pattern2.js
@@ -0,0 +1,19 @@
+/**
+ * *
+ * * *
+ * * * *
+ * * * * *
+ */
+
+
+const n = 6;
+for(let i =0; i<n; i++){
+    let row = ""
+    for(let j=0; j<i+1; j++){
+       
+       row += "* "  
+    }
+    console.log(row);
+   
+    
+}
